d86be37b766fc288af59e13bd04e38cada803efa
abarthel11 <austinbarthel@gmail.com>
2025-02-07 10:56:45 -0700

    format

--- lwc/lwc/mixins/lwc/myComponent/myComponent.js
+++ lwc/lwc/mixins/lwc/myComponent/myComponent.js
@@ -4,19 +4,19 @@ import { LoggerMixin } from 'c/loggerMixin';
 import { NavigationMixin } from 'lightning/navigation';
 
 export default class MyComponent extends LoggerMixin(
-  NavigationMixin(LightningElement)
+	NavigationMixin(LightningElement)
 ) {
-  connectedCallback() {
-    this[NavigationMixin.GenerateUrl]({
-        type: 'standard__recordPage',
-        attributes: {
-          recordId: '005B0000001ptf1IAE',
-          actionName: 'view',
-        },
-    }).then((url) => {
-      this.log(`Generated Url: ${url}`);
-    })
-  }
+	connectedCallback() {
+		this[NavigationMixin.GenerateUrl]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: '005B0000001ptf1IAE',
+				actionName: 'view',
+			},
+		}).then((url) => {
+			this.log(`Generated Url: ${url}`);
+		})
+	}
 
 	handleError() {
 		this.error('An Error occured');
